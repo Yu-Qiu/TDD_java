b9087a67f6fab2288d3ae013609d101650716d9e
YuQiu <yu.qiu@centrale-marseille.fr>
2015-01-09 19:16:17 +0100

    chapitre 5: Franc

---
+++ src/main/Franc.java
@@ -0,0 +1,23 @@
+package main;
+
+/**
+ * Created by YuQiu on 09/01/15.
+ */
+public class Franc {
+
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier){
+        return new Franc(amount*multiplier);
+    }
+
+    public boolean equals(Object object){
+        Franc Franc = (Franc) object;
+        return amount == Franc.amount;
+    }
+
+}
--- src/tests/DollarTest.java
+++ src/tests/DollarTest.java
@@ -3,6 +3,7 @@ package tests;
 import main.Dollar;
 import org.junit.Test;
 
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
