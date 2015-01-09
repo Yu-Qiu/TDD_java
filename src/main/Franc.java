45b6ede671a26de4ad33caa455f981284774a817
YuQiu <yu.qiu@centrale-marseille.fr>
2015-01-09 21:24:23 +0100

    chapitre 8: creer Money.dollar et Money.franc

--- src/main/Franc.java
+++ src/main/Franc.java
@@ -9,8 +9,8 @@ public class Franc extends Money {
         this.amount = amount;
     }
 
-    public Franc times(int multiplier){
-        return new Franc(amount*multiplier);
+    public Money times(int multiplier){
+        return new Franc (amount * multiplier);
     }
 
 }
--- src/main/Money.java
+++ src/main/Money.java
@@ -3,13 +3,23 @@ package main;
 /**
  * Created by YuQiu on 09/01/15.
  */
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    public abstract Money times(int multiplier);
 
 }
