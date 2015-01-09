cbc2ba4697d86c863c3f57df769444c836ce20a1
YuQiu <yu.qiu@centrale-marseille.fr>
2015-01-09 18:46:12 +0100

    chapitre 3: egalite

--- src/tests/DollarTest.java
+++ src/tests/DollarTest.java
@@ -4,6 +4,8 @@ import main.Dollar;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 public class DollarTest {
@@ -15,6 +17,12 @@ public class DollarTest {
         assertEquals(10, product.amount);
         Dollar product2 = five.times(3);
         assertEquals(15, product2.amount);
+    }
+
+    @Test
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
 }
