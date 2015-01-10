cd20170b701629851594c4b3496848a83b2282e8
YuQiu <yu.qiu@centrale-marseille.fr>
2015-01-10 13:00:16 +0100

    chapitre 14: add rate in bank

--- src/main/Bank.java
+++ src/main/Bank.java
@@ -1,11 +1,25 @@
 package main;
 
+import java.util.Hashtable;
+
 /**
  * Created by YuQiu on 09/01/15.
  */
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        Integer rate= (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    private Hashtable rates= new Hashtable();
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
--- src/main/Expression.java
+++ src/main/Expression.java
@@ -5,6 +5,6 @@ package main;
  */
 public interface Expression {
 
-    Money reduce(String to);
+    Money reduce(Bank bank, String to);
 
 }
