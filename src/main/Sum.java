552b488d1e7fa0f4ce46c2000b1edd7a6c7648d0
YuQiu <yu.qiu@centrale-marseille.fr>
2015-01-10 13:11:57 +0100

    chapitre 15: complicated add

--- src/main/Sum.java
+++ src/main/Sum.java
@@ -4,16 +4,20 @@ package main;
  * Created by YuQiu on 09/01/15.
  */
 public class Sum implements Expression{
-    public Money augend;
-    public Money addend;
+    public Expression augend;
+    public Expression addend;
 
-    public Sum(Money augend, Money addend){
+    public Sum(Expression augend, Expression addend){
         this.augend = augend;
         this.addend = addend;
     }
 
     public Money reduce(Bank bank, String to){
-        int amount = augend.amount + addend.amount;
+        int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
+    }
+
+    public Expression plus(Expression addend){
+        return null;
     }
 }
